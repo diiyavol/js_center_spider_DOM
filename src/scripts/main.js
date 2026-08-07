f513a289ff7da9d45eec3c38dd788eb37cf2cade
diiyavol <nikitanesterenko88@gmail.com>
2026-08-07 12:24:31 +0300

    Solution 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const elemWall = document.querySelector('.wall');
+const rectWall = elemWall.getBoundingClientRect();
+const elemSpider = document.querySelector('.spider');
+const rectSpider = elemSpider.getBoundingClientRect();
+
+elemSpider.style.left = (rectWall.width - rectSpider.width) / 2 + 'px';
+elemSpider.style.top = (rectWall.height - rectSpider.height) / 2 + 'px';
