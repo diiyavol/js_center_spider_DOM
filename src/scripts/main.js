25854d413674c2c7d12f364b8b8c869072c82e2b
diiyavol <nikitanesterenko88@gmail.com>
2026-08-07 12:48:28 +0300

    solution 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,23 @@
 'use strict';
 
 const elemWall = document.querySelector('.wall');
-// const rectWall = elemWall.getBoundingClientRect();
+const rectWall = elemWall.getBoundingClientRect();
 
 const elemSpider = document.querySelector('.spider');
+const rectSpider = elemSpider.getBoundingClientRect();
 
-const spiderHeight = elemSpider.offsetHeight;
-const spiderWidth = elemSpider.offsetWidth;
-const wallHeight = elemWall.clientHeight;
-const wallWidth = elemWall.clientWidth;
+elemSpider.style.left = (rectWall.width - rectSpider.width) / 2 - 10 + 'px';
 
-const leftCoord = wallWidth / 2 - spiderWidth / 2;
-const topCoord = wallHeight / 2 - spiderHeight / 2;
+elemSpider.style.top = (rectWall.height - rectSpider.height) / 2 - 10 + 'px';
 
-elemSpider.style.left = `${leftCoord}px`;
-elemSpider.style.top = `${topCoord}px`;
-elemSpider.style.position = 'absolute';
+// const spiderHeight = elemSpider.offsetHeight;
+// const spiderWidth = elemSpider.offsetWidth;
+// const wallHeight = elemWall.clientHeight;
+// const wallWidth = elemWall.clientWidth;
 
-// const rectSpider = elemSpider.getBoundingClientRect();
+// const leftCoord = wallWidth / 2 - spiderWidth / 2;
+// const topCoord = wallHeight / 2 - spiderHeight / 2;
 
-// elemSpider.style.left = (rectWall.width - rectSpider.width) / 2 + 'px';
-
-// elemSpider.style.top = (rectWall.height - rectSpider.height) / 2 + 'px';
+// elemSpider.style.left = `${leftCoord}px`;
+// elemSpider.style.top = `${topCoord}px`;
+// elemSpider.style.position = 'absolute';
